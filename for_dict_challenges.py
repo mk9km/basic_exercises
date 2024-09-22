fb2751c10ab430d3d686051115b5573629bd7dfb
mk9km <mad_matt@mail.ru>
2024-09-22 23:58:01 +0300

    Add for, dict, string challenges

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -1,10 +1,16 @@
+from typing import Dict, List, Tuple
+
+from utils import delimiter
+
+# -------------------------------------------------------
+
 # Задание 1
 # Дан список учеников, нужно посчитать количество повторений каждого имени ученика
 # Пример вывода:
 # Вася: 1
 # Маша: 2
 # Петя: 2
-
+delimiter()
 students = [
     {'first_name': 'Вася'},
     {'first_name': 'Петя'},
@@ -12,13 +18,22 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Петя'},
 ]
-# ???
 
+rv = {}
+for student in students:
+    name = student['first_name']
+    rv[name] = rv.get(name, 0) + 1
+
+for name, num in rv.items():
+    print(f'{name}: {num}')
+
+# -------------------------------------------------------
 
 # Задание 2
 # Дан список учеников, нужно вывести самое часто повторящееся имя
 # Пример вывода:
 # Самое частое имя среди учеников: Маша
+delimiter()
 students = [
     {'first_name': 'Вася'},
     {'first_name': 'Петя'},
@@ -26,15 +41,31 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Оля'},
 ]
-# ???
 
+def get_student_with_max_name_repeat_in_class(cls: List[Dict[str, str]]) -> Tuple:
+    rv = {}
+    max_student, max_num = '', 0
+    for student in students:
+        name = student['first_name']
+        rv[name] = rv.get(name, 0) + 1
+
+        if max_num < rv[name]:
+            max_student, max_num = name, rv[name]
+
+    return max_student, max_num
+
+max_student, max_num = get_student_with_max_name_repeat_in_class(students)
+
+print(f'Самое частое имя среди учеников: {max_student}')
+
+# -------------------------------------------------------
 
 # Задание 3
 # Есть список учеников в нескольких классах, нужно вывести самое частое имя в каждом классе.
 # Пример вывода:
 # Самое частое имя в классе 1: Вася
 # Самое частое имя в классе 2: Маша
-
+delimiter()
 school_students = [
     [  # это – первый класс
         {'first_name': 'Вася'},
@@ -51,15 +82,19 @@ school_students = [
         {'first_name': 'Саша'},
     ],
 ]
-# ???
 
+for cls_num, students in enumerate(school_students, start=1):
+    max_student, max_num = get_student_with_max_name_repeat_in_class(students)
+    print(f'Самое частое имя в классе {cls_num}: {max_student}')
+
+# -------------------------------------------------------
 
 # Задание 4
 # Для каждого класса нужно вывести количество девочек и мальчиков в нём.
 # Пример вывода:
 # Класс 2a: девочки 2, мальчики 0 
 # Класс 2б: девочки 0, мальчики 2
-
+delimiter()
 school = [
     {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
     {'class': '2б', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
@@ -72,15 +107,46 @@ is_male = {
     'Миша': True,
     'Даша': False,
 }
-# ???
 
+def get_gender_by_class_in_school(
+        school: List[Dict[str, str]], is_male: Dict[str, bool], print_out: bool = False
+) -> List[Dict[str, str]]:
+    rv = []
+    for cls in school:
+        class_name = cls['class']
+        students = cls['students']
+
+        female_num, male_num = 0, 0
+        for student in students:
+            name = student['first_name']
+
+            if is_male[name]:
+                male_num += 1
+            else:
+                female_num += 1
+
+        gender_by_class = {
+            'cls_name': class_name,
+            'male_num': male_num,
+            'female_num': female_num
+        }
+        rv.append(gender_by_class)
+
+        if print_out:
+            print('Класс {cls_name}: девочки {female_num}, мальчики {male_num}'.format(**gender_by_class))
+
+    return rv
+
+get_gender_by_class_in_school(school, is_male, True)
+
+# -------------------------------------------------------
 
 # Задание 5
 # По информации о учениках разных классов нужно найти класс, в котором больше всего девочек и больше всего мальчиков
 # Пример вывода:
 # Больше всего мальчиков в классе 3c
 # Больше всего девочек в классе 2a
-
+delimiter()
 school = [
     {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
     {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
@@ -91,5 +157,20 @@ is_male = {
     'Олег': True,
     'Миша': True,
 }
-# ???
 
+max_male_cls, max_male_num = '', 0
+max_female_cls, max_female_num = '', 0
+gender_by_class = get_gender_by_class_in_school(school, is_male)
+# NOTE: more optimal way is to integrate these check into 'get_gender_by_class_in_school' function, but this
+# approach is more human friendly, by my opinion (this is not BigData, right?).
+for cls in gender_by_class:
+    if max_male_num < cls['male_num']:
+        max_male_num = cls['male_num']
+        max_male_cls = cls['cls_name']
+
+    if max_female_num < cls['female_num']:
+        max_female_num = cls['female_num']
+        max_female_cls = cls['cls_name']
+
+print(f'Больше всего мальчиков в классе {max_male_cls}')
+print(f'Больше всего девочек в классе {max_female_cls}')
--- string_challenges.py
+++ string_challenges.py
@@ -1,28 +1,36 @@
+from utils import delimiter
+
+delimiter()
 # Вывести последнюю букву в слове
 word = 'Архангельск'
-# ???
+print(word[-1])
 
 
+delimiter()
 # Вывести количество букв "а" в слове
 word = 'Архангельск'
-# ???
+print(word.count('а'))
 
 
+delimiter()
 # Вывести количество гласных букв в слове
 word = 'Архангельск'
-# ???
+print(len([x for x in word if x in 'АЕЁИОУЫЭЮЯаеёиоуыэюя']))
 
 
+delimiter()
 # Вывести количество слов в предложении
 sentence = 'Мы приехали в гости'
-# ???
+print(len([w for w in sentence.split(' ') if w]))
 
 
+delimiter()
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
-# ???
+print(*[w for w in sentence.split(' ') if w], sep='\n')
 
 
+delimiter()
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
-# ???
+print(sum([len(w) for w in sentence.split(' ') if w])/len([len(w) for w in sentence.split(' ') if w]))
